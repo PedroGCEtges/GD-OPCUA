97db9a58acfbbd468979712ebb42ea7c9ca6a9a8
PedroGCEtges <pedroetges11@gmail.com>
2023-12-08 20:01:18 -0300

    add sqlite tests

---
+++ test_sqlite/test4_sqlite.py
@@ -0,0 +1,42 @@
+import sqlite3
+import pandas as pd
+
+conn = sqlite3.connect("opcua.db")
+cursor = conn.cursor()
+
+cursor.execute("SELECT tag, GROUP_CONCAT(value) AS value, GROUP_CONCAT(timestamp) AS timestamps FROM opcua_data GROUP BY tag")
+# Obter os resultados da consulta
+results = cursor.fetchall()
+# Iterar sobre as tags únicas do resultado
+for row in results:
+    try:
+        # Obter o nome da tag
+        tag = row[0]
+        # Separar os valores e os timestamps por vírgula
+        values = row[1].split(",")
+        timestamps = row[2].split(",")
+        if not(tag == "0" or tag == "1"):
+            
+            # Criar uma nova tabela com o nome da tag
+            cursor.execute(f"CREATE TABLE \"{tag}\"  (value INTEGER, timestamp TEXT)")
+            # Inserir os valores e os timestamps na nova tabela
+            for i in range(len(values)):
+                cursor.execute(f"INSERT INTO \"{tag}\" VALUE (?, ?)", (values[i], timestamps[i]))
+    
+    except sqlite3.OperationalError as e:
+        if str(e) == f'table "{tag}" already exists':
+            for i in range(len(values)):
+                cursor.execute(f"INSERT INTO {tag} VALUES (?, ?)", (values[i], timestamps[i]))
+        else:
+            print(e)
+            
+# Salvar as alterações no banco de dados
+conn.commit()
+
+df = pd.read_sql('SELECT * FROM G3 ORDER BY timestamp ASC', conn)
+df.drop_duplicates(inplace=True)
+df.reset_index(drop=True, inplace=True)
+df.to_sql('opcua_data_filtered', conn)
+print(df.head())
+
+conn.close()
